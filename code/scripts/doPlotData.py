132961d83e43041c8c18eb56ac1eebf7166cc4f8
joacorapela <joacorapela@gmail.com>
2025-01-27 04:33:57 +0000

    Saving figure

--- code/scripts/doPlotData.py
+++ code/scripts/doPlotData.py
@@ -11,10 +11,14 @@ def main(argv):
                         default="Cz")
     parser.add_argument("--data_filename", type=str, help="data filename",
                         default="../../../data/D_01_cleaned.mat")
+    parser.add_argument("--fig_filename_pattern", type=str,
+                        help="figure filename pattern",
+                        default="../../figures/D_01_cleaned_{ch}.{ext}")
     args = parser.parse_args()
 
     channel_label = args.channel_label
     data_filename = args.data_filename
+    fig_filename_pattern = args.fig_filename_pattern
 
     mat = scipy.io.loadmat(data_filename)
     data = mat["data"]
@@ -38,6 +42,9 @@ def main(argv):
         fig.add_trace(trace)
     fig.update_layout(xaxis_title="Time (msec)", yaxis_title="Voltage")
     fig.show()
+
+    fig.write_image(fig_filename_pattern.format(ch=channel_label, ext="png"))
+    fig.write_html(fig_filename_pattern.format(ch=channel_label, ext="html"))
 
     breakpoint()
 
